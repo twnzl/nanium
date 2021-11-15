aa6cad401d83917f9ae029f16dac6c5abf04f03e
twnzl <work@die-wenzlinge.de>
2021-11-15 13:45:21 +0100

    isResponsible: default 'yes' instead of required

--- src/interfaces/serviceConsumerConfig.ts
+++ src/interfaces/serviceConsumerConfig.ts
@@ -5,6 +5,6 @@ import { NaniumSerializer } from './serializer';
 export interface ServiceConsumerConfig {
 	requestInterceptors?: ServiceRequestInterceptor<any>[];
 	handleError?: (e: any) => Promise<void>;
-	isResponsible: (request: any, serviceName: string) => Promise<KindOfResponsibility>;
+	isResponsible?: (request: any, serviceName: string) => Promise<KindOfResponsibility>;
 	serializer?: NaniumSerializer;
 }
