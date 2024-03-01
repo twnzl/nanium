e183a008f386cfd0d8eca6e25308bd065f18c331
Tobias Wenzel <tobias@syscore.io>
2024-03-01 13:20:06 +0100

    class NaniumStream + tests

--- _src/tests/services/test/query.contract.ts
+++ _src/tests/services/test/query.contract.ts
@@ -9,6 +9,11 @@ export class TestDto {
 	@Type(String) a: string;
 	@Type(Number) b: number;
 
+	constructor(a: string, b: number) {
+		this.a = a;
+		this.b = b;
+	}
+
 	formatted(): string {
 		return this.a + ':' + this.b;
 	}
